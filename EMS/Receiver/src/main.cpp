d6fcf20a531aa564f7e6004662be8d8daf3049ec
Fal318 <fal.falco318@gmail.com>
2021-09-30 17:31:32 +0900

    :bug: Fix bug.

--- EMS/Receiver/src/main.cpp
+++ EMS/Receiver/src/main.cpp
@@ -2,27 +2,24 @@
 #include "mbed.h"
 
 DigitalOut myled(LED1);
-PwmOut ems_out(A6);
+DigitalOut ems_out(A6);
 Im920 im920(D1, D0, 19200);
 UnbufferedSerial pc(USBTX, USBRX);
 
-int main()
-{
+int main() {
+
   uint8_t lastData = 0;
-  while (true)
-  {
+  while (true) {
+
     Im920Output readVal = im920.recv();
-    if (readVal.isSuccess && (readVal.data[0] == 255 || lastData == 255))
-    {
-      ems_out = 0.36;
+    if ((readVal.isSuccess && readVal.data[0] == 255) || lastData == 255) {
+      ems_out = 1;
       myled = 1;
-    }
-    else
-    {
+    } else {
       ems_out = 0;
       myled = 0;
     }
     lastData = readVal.data[0];
-    ThisThread::sleep_for(chrono::milliseconds(50));
+    ThisThread::sleep_for(chrono::milliseconds(150));
   }
 }
