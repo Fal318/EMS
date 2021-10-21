8316159c0beb2f8600e3f5416c2f8fa84025f92f
Fal318 <fal.falco318@gmail.com>
2021-10-21 14:00:15 +0900

    :sparkles:Mbed OS6への移行

--- EMS/Receiver/src/main.cpp
+++ EMS/Receiver/src/main.cpp
@@ -1,25 +1,30 @@
 #include "IM920/im920.h"
 #include "mbed.h"
 
+using namespace std;
+
 DigitalOut myled(LED1);
-DigitalOut ems_out(A6);
+PwmOut ems_out(A6);
 Im920 im920(D1, D0, 19200);
 UnbufferedSerial pc(USBTX, USBRX);
 
-int main() {
-
+int main()
+{
   uint8_t lastData = 0;
-  while (true) {
-
+  while (true)
+  {
     Im920Output readVal = im920.recv();
-    if ((readVal.isSuccess && readVal.data[0] == 255) || lastData == 255) {
-      ems_out = 1;
-      myled = 1;
-    } else {
-      ems_out = 0;
-      myled = 0;
+    if ((readVal.isSuccess && readVal.data[0] == 255) || lastData == 255)
+    {
+      ems_out.write(0.36f);
+      myled.write(1);
+    }
+    else
+    {
+      ems_out.write(0.f);
+      myled.write(0);
     }
     lastData = readVal.data[0];
-    ThisThread::sleep_for(chrono::milliseconds(150));
+    ThisThread::sleep_for(150ms);
   }
 }
